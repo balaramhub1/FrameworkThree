657bf348da724fbf806c6fbf0dd1c382e852ca09
balaram <balaram1985behera@gmail.com>
2018-08-08 12:41:00 +0530

    initial commit for branch1_1

--- src/test/java/homePage/TestClass2.java
+++ src/test/java/homePage/TestClass2.java
@@ -1,12 +1,18 @@
 package homePage;
 
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TestClass2 {
 	
 	@BeforeTest
 	public void beforeTest() {
-		System.out.println("Test Class one");
+		System.out.println("Before Test Class one");
+	}
+	
+	@Test
+	public void TestOne() {
+		System.out.println("Test Class ONe");
 	}
 
 }
